6a3803747f886dfa3f8166a19616104b3850688e
Apoorv <apoorvrastogi58@gmail.com>
2021-05-23 22:04:07 +0530

    Amazon Header(NavBar)

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBuNdaz3WKYKbYe-c_AT--963dLFnP5isA",
+  authDomain: "clone-fullv.firebaseapp.com",
+  projectId: "clone-fullv",
+  storageBucket: "clone-fullv.appspot.com",
+  messagingSenderId: "286065041544",
+  appId: "1:286065041544:web:f5803e05841f3644f97f3d",
+  measurementId: "G-BLY34EQ86Z",
+};
